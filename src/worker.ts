b1a098eca66d8e8120aac7b38fd95ba6fbbda3ef
Smoren <ofigate@gmail.com>
2025-01-04 21:01:39 +0300

    worker.ts added.

---
+++ src/worker.ts
@@ -0,0 +1,10 @@
+process.on('message', async (message: any) => {
+  const { taskFunctionString, inputData, taskIndex } = message;
+  const taskFunction = eval(`(${taskFunctionString})`);
+  try {
+    const result = await taskFunction(inputData);
+    process.send!({ result, inputData, taskIndex });
+  } catch (error) {
+    process.send!({ error: error!.toString(), inputData, taskIndex });
+  }
+});
