1ddfd4699804058d0214844379ba0d89349aa69f
Smoren <ofigate@gmail.com>
2025-01-04 21:33:53 +0300

    Docs added.

--- README.md
+++ README.md
@@ -12,6 +12,8 @@ Description
 
 Multiprocessing pool implementation for TypeScript.
 
+Real multiprocessing is implemented using [child_process](https://nodejs.org/api/child_process.html) module.
+
 Setup
 -----
 
@@ -22,7 +24,123 @@ npm i multiprocessor
 Usage example
 -------------
 
-Under construction....
+```typescript
+import { Pool } from 'multiprocessor';
+
+const poolSize = 4;
+
+const pool = new Pool(poolSize);
+const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const onItemResult = (itemResult: number, itemInput: number, taskIndex: number) => {
+  console.log('itemResult', itemResult, itemInput, taskIndex);
+};
+const onItemError = (error: string, itemInput: number, taskIndex: number) => {
+  console.log('itemError', error, itemInput, taskIndex);
+};
+
+const result = await pool.map(input, calcSinTask, onItemResult, onItemError);
+pool.close();
+
+console.log(result);
+// [ 0.8414, 0.9092, 0.1411, ... ]
+
+function calcSinTask(x: number): number {
+  let result = 0;
+  let sign = 1;
+  let power = x;
+  let factorial = 1;
+
+  for (let n = 0; n < 1000000; n++) {
+    if (n > 0) {
+      factorial *= (2 * n) * (2 * n + 1);
+      power *= x * x;
+      sign *= -1;
+    }
+
+    const delta = sign * (power / factorial);
+
+    if (isNaN(result + delta)) {
+      return result
+    }
+
+    result += delta;
+  }
+
+  return result;
+}
+```
+
+API Reference
+-------------
+
+## Types
+```typescript
+export type Task<TInput, TResult> = ((input: TInput) => TResult) | ((input: TInput) => Promise<TResult>)
+export type ItemResultHandler<TInput, TResult> = (itemResult: TResult, itemInput: TInput, taskIndex: number) => void;
+export type ItemErrorHandler<TInput> = (error: string, itemInput: TInput, taskIndex: number) => void;
+```
+
+## Pool
+```typescript
+class Pool extends EventEmitter {
+  /**
+   * Create a new pool with the specified number of workers.
+   *
+   * @param poolSize The number of workers to create in the pool.
+   */
+  constructor(poolSize: number);
+
+  /**
+   * Asynchronously processes tasks from the provided inputs in a lazy unordered manner.
+   * Tasks are executed concurrently using a pool of workers.
+   *
+   * @template TInput The type of the input elements.
+   * @template TResult The type of the result elements.
+   *
+   * @param inputs An iterable or async iterable of input elements.
+   * @param task The task to execute for each input element.
+   * @param onItemResult Optional callback invoked when a task completes successfully.
+   * @param onItemError Optional callback invoked when a task encounters an error.
+   *
+   * @returns An async generator yielding results of the tasks in completion order.
+   */
+  public async *imapUnordered<TInput, TResult>(
+    inputs: Iterable<TInput> | AsyncIterable<TInput>,
+    task: Task<TInput, TResult>,
+    onItemResult?: ItemResultHandler<TInput, TResult>,
+    onItemError?: ItemErrorHandler<TInput>,
+  ): AsyncGenerator<TResult | undefined>;
+
+  /**
+   * Asynchronously processes tasks from the provided inputs in an ordered manner.
+   * Tasks are executed concurrently using a pool of workers.
+   *
+   * @template TInput The type of the input elements.
+   * @template TResult The type of the result elements.
+   *
+   * @param inputs An iterable or async iterable of input elements.
+   * @param task The task to execute for each input element.
+   * @param onItemResult Optional callback invoked when a task completes successfully.
+   * @param onItemError Optional callback invoked when a task encounters an error.
+   *
+   * @returns A promise that resolves to an array of task results in the order of the input elements.
+   */
+  public async map<TInput, TResult>(
+    inputs: Iterable<TInput> | AsyncIterable<TInput>,
+    task: Task<TInput, TResult>,
+    onItemResult?: ItemResultHandler<TInput, TResult>,
+    onItemError?: ItemErrorHandler<TInput>,
+  ): Promise<Array<TResult | undefined>>;
+
+  /**
+   * Closes the worker pool by terminating all worker processes.
+   * This method should be called when the pool is no longer needed
+   * to ensure that all resources are properly released.
+   */
+  public close();
+}
+```
 
 Unit testing
 ------------
--- src/pool.ts
+++ src/pool.ts
@@ -3,15 +3,45 @@ import { EventEmitter } from 'events';
 import { fork, ChildProcess } from 'child_process';
 import type { ItemErrorHandler, ItemResultHandler, ResultMessage, Task } from "./types";
 
+/**
+ * A class representing a pool of workers for executing tasks concurrently using child processes.
+ * Extends EventEmitter to allow task result events to be emitted.
+ */
 export class Pool extends EventEmitter {
+  /**
+   * Array of child processes representing the workers in the pool.
+   */
   private workers: ChildProcess[] = [];
+  /**
+   * Array of child processes representing the available workers in the pool.
+   */
   private availableWorkers: ChildProcess[] = [];
+  /**
+   * Array of tasks to be processed in the pool.
+   */
   private taskQueue: Array<{ inputData: any; taskFunctionString: string }> = [];
+  /**
+   * Map of tasks currently being processed by workers, keyed by worker.
+   */
   private tasksInProcess = new Map<ChildProcess, any>();
+  /**
+   * Current task index, incremented for each task processed.
+   */
   private currentTaskIndex = 0;
+  /**
+   * Optional callback invoked when a task completes successfully.
+   */
   private onItemResult: ItemResultHandler<any, any>;
+  /**
+   * Optional callback invoked when a task encounters an error.
+   */
   private onItemError: ItemErrorHandler<any>;
 
+  /**
+   * Create a new pool with the specified number of workers.
+   *
+   * @param poolSize The number of workers to create in the pool.
+   */
   constructor(poolSize: number) {
     super();
     this.initWorkers(poolSize);
@@ -19,6 +49,20 @@ export class Pool extends EventEmitter {
     this.onItemError = this.createEmptyHandler();
   }
 
+  /**
+   * Asynchronously processes tasks from the provided inputs in a lazy unordered manner.
+   * Tasks are executed concurrently using a pool of workers.
+   *
+   * @template TInput The type of the input elements.
+   * @template TResult The type of the result elements.
+   *
+   * @param inputs An iterable or async iterable of input elements.
+   * @param task The task to execute for each input element.
+   * @param onItemResult Optional callback invoked when a task completes successfully.
+   * @param onItemError Optional callback invoked when a task encounters an error.
+   *
+   * @returns An async generator yielding results of the tasks in completion order.
+   */
   public async *imapUnordered<TInput, TResult>(
     inputs: Iterable<TInput> | AsyncIterable<TInput>,
     task: Task<TInput, TResult>,
@@ -30,6 +74,20 @@ export class Pool extends EventEmitter {
     }
   }
 
+  /**
+   * Asynchronously processes tasks from the provided inputs in an ordered manner.
+   * Tasks are executed concurrently using a pool of workers.
+   *
+   * @template TInput The type of the input elements.
+   * @template TResult The type of the result elements.
+   *
+   * @param inputs An iterable or async iterable of input elements.
+   * @param task The task to execute for each input element.
+   * @param onItemResult Optional callback invoked when a task completes successfully.
+   * @param onItemError Optional callback invoked when a task encounters an error.
+   *
+   * @returns A promise that resolves to an array of task results in the order of the input elements.
+   */
   public async map<TInput, TResult>(
     inputs: Iterable<TInput> | AsyncIterable<TInput>,
     task: Task<TInput, TResult>,
@@ -44,12 +102,31 @@ export class Pool extends EventEmitter {
     return result.map((item) => item[1]);
   }
 
+  /**
+   * Closes the worker pool by terminating all worker processes.
+   * This method should be called when the pool is no longer needed
+   * to ensure that all resources are properly released.
+   */
   public close() {
     for (const worker of this.workers) {
       worker.kill();
     }
   }
 
+  /**
+   * Asynchronously processes tasks from the provided inputs in a lazy unordered manner with task indices.
+   * Tasks are executed concurrently using a pool of workers.
+   *
+   * @template TInput The type of the input elements.
+   * @template TResult The type of the result elements.
+   *
+   * @param inputs An iterable or async iterable of input elements.
+   * @param task The task to execute for each input element.
+   * @param onItemResult Optional callback invoked when a task completes successfully.
+   * @param onItemError Optional callback invoked when a task encounters an error.
+   *
+   * @returns An async generator yielding task results.
+   */
   private async *mapTasks<TInput, TResult>(
     inputs: Iterable<TInput> | AsyncIterable<TInput>,
     task: Task<TInput, TResult>,
