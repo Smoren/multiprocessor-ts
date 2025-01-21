a90e2684a6231979b6537e1d2a146997ab0f331d
Smoren <ofigate@gmail.com>
2025-01-21 10:34:10 +0300

    Fix for import types.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import type { TaskResponse, TaskSuccessHandler, TaskErrorHandler } from "./types";
+import type { TaskResponse, TaskSuccessHandler, TaskErrorHandler, TaskHandlers } from "./types";
 import { Pool } from "./pool";
 
 export type {
   TaskResponse,
   TaskSuccessHandler,
   TaskErrorHandler,
+  TaskHandlers,
 };
 
 export {
